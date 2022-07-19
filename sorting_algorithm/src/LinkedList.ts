9f3e608a4ff850d0129cb631f8b6fc0d9b727ea9
KamilJakobczak <jamardracken@gmail.com>
2022-07-19 10:23:16 +0200

    use abstract class

--- sorting_algorithm/src/LinkedList.ts
+++ sorting_algorithm/src/LinkedList.ts
@@ -1,9 +1,11 @@
+import { Sorter } from './Sorter';
+
 class SingleNode {
   next: SingleNode | null = null;
   constructor(public data: number) {}
 }
 
-export class LinkedList {
+export class LinkedList extends Sorter {
   head: SingleNode | null = null;
 
   add(data: number): void {
@@ -17,6 +19,7 @@ export class LinkedList {
     while (tail.next) {
       tail = tail.next;
     }
+    tail.next = newNode;
   }
   get length(): number {
     if (!this.head) {
@@ -29,5 +32,50 @@ export class LinkedList {
       node = node.next;
     }
     return length;
+  }
+
+  at(index: number): SingleNode {
+    if (!this.head) {
+      throw new Error('Index out of bounds');
+    }
+
+    let counter = 0;
+    let node: SingleNode | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+    throw new Error('Index out of bounds');
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error('List is empty');
+    }
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+    let node: SingleNode | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
   }
 }
