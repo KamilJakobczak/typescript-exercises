96735ef55c44ce00fae2f4ba84d1de3e913eb5c1
KamilJakobczak <jamardracken@gmail.com>
2022-08-04 10:29:44 +0200

    inheritance vs composition

--- stats_app/src/index.ts
+++ stats_app/src/index.ts
@@ -1,16 +1,19 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
-console.log(reader.data);
+import { MatchResult } from './MatchResult';
+
+const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-// for (let match of reader.data) {
-//   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-//     manUnitedWins++;
-//   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-//     manUnitedWins++;
-//   }
-// }
-// console.log(`Manchaester United won ${manUnitedWins} games`);
+for (let match of matchReader.matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+console.log(`Manchaester United won ${manUnitedWins} games`);
