2a3f74eb4685fc155efd931d48638acca4289585
Kamil Jakóbczak <k_j1@vp.pl>
2022-08-09 12:34:30 +0200

    implement generic View class

--- web_framework/src/index.ts
+++ web_framework/src/index.ts
@@ -1,7 +1,10 @@
 import { User } from './models/User';
+import { UserForm } from './views/UserForm';
 
-const collection = User.buildUserCollection();
-collection.on('change', () => {
-  console.log(collection);
-});
-collection.fetch();
+const element = document.getElementById('root');
+const user = User.buildUser({ name: 'Gabi', age: 3 });
+if (!element) {
+} else {
+  const userForm = new UserForm(element, user);
+  userForm.render();
+}
