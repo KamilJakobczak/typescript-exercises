e3a0c385593264053645aa6b4f02b37fd7bc661c
Kamil Jakóbczak <k_j1@vp.pl>
2022-08-11 10:16:19 +0200

    complete application

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Working');
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import express from 'express';
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieSession({ keys: ['adgeqwugdf'] }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
